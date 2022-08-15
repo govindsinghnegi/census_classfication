58da4eb6c3aebb8ef085bba37cc24883b0bd5818
gsn <gsn@example.com>
2022-08-15 10:00:59 -0700

    add CensusData BaseModel and add demo for post request

---
+++ demo_fastapi.py
@@ -0,0 +1,23 @@
+import requests
+
+census_data = {
+    'age': 30,
+    'workclass': 'Private',
+    'fnlwgt': 12345,
+    'education': 'Bachelors',
+    'education-num': 13,
+    'marital_status': 'Separated',
+    'occupation': 'Sales',
+    'relationship': 'Unmarried',
+    'race': 'Black',
+    'sex': 'Male',
+    'capital_gain': 0,
+    'capital_loss': 0,
+    'hours_per_week': 40,
+    'native_country': 'Mexico'
+}
+
+response = requests.post('https://census-classifcation.herokuapp.com/create_prediction', json=data)
+
+print(f'response code: {response.status_code}')
+print(f'response body: {response.json()}')
--- fastapi_impl.py
+++ fastapi_impl.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+class CensusData(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: str
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: male
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
 app = FastAPI()
 
 @app.get("/")
 async def say_hello():
     return {"greeting": "Hello World!"}
+
+@app.post("/create_prediction")
+async def exercise_function(census_data: CensusData):
+    print(f'--- post called-----')
+    print(f'age: {census_data.age}')
+    print(f'workclass: {census_data.workclass}')
+    print(f'marital_status: {census_data.marital_status}')
+    print(f'race: {census_data.race}')
+    print(f'hours_per_week: {census_data.hours_per_week}')
+    print(f'native_country: {census_data.native_country}')
+    return census_data
