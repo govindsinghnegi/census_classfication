1c4e59fb4293ea813bb9a6d658b1766e490ceccc
gsn <gsn@example.com>
2022-08-15 23:17:19 -0700

    use existing model to create the prediction

--- fastapi_impl.py
+++ fastapi_impl.py
@@ -1,5 +1,11 @@
+import os
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from starter.ml.data import process_data
+from starter.ml.model import inference
+from starter.train_model import load_model
+
 
 class CensusData(BaseModel):
     age: int = Field(example=30)
@@ -26,10 +32,54 @@ async def say_hello():
 @app.post("/create_prediction")
 async def exercise_function(census_data: CensusData):
     print(f'--- post called-----')
-    print(f'age: {census_data.age}')
-    print(f'workclass: {census_data.workclass}')
-    print(f'marital_status: {census_data.marital_status}')
-    print(f'race: {census_data.race}')
-    print(f'hours_per_week: {census_data.hours_per_week}')
-    print(f'native_country: {census_data.native_country}')
-    return census_data
+    current_dir = os.path.dirname(__file__)
+    encoder_path = os.path.join(current_dir, 'model/encoder.pkl')
+    lb_path = os.path.join(current_dir, 'model/lb.pkl')
+    model_path = os.path.join(current_dir, 'model/model.pkl')
+
+    saved_model = load_model(model_path)
+    saved_encoder = load_model(encoder_path)
+    saved_lb = load_model(lb_path)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    census_data_dict = {
+        'age': census_data.age,
+        'workclass': census_data.workclass,
+        'fnlgt': census_data.fnlgt,
+        'education': census_data.education,
+        'education-num': census_data.education_num,
+        'marital-status': census_data.marital_status,
+        'occupation': census_data.occupation,
+        'relationship': census_data.relationship,
+        'race': census_data.race,
+        'sex': census_data.sex,
+        'capital-gain': census_data.capital_gain,
+        'capital-loss': census_data.capital_loss,
+        'hours-per-week': census_data.hours_per_week,
+        'native-country': census_data.native_country,
+    }
+
+    df = pd.DataFrame(census_data_dict)
+
+    print(df.shape)
+
+    X, _, _, _ = process_data(df, categorical_features=cat_features, label='salary',
+                                        training=False, encoder=saved_encoder, lb=saved_lb)
+
+    logger.info(f'X shape: {X.shape}')
+
+    preds = inference(saved_model, X)
+
+    print(f'preds: {preds[0]}')
+
+    return {'prediction': preds[0]}
